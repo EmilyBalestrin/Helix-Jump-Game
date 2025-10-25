878b5673a86fd9149ab899584e68f18bbf27e336
Emily L. Balestrin <emilyl.balestrin@gmail.com>
2025-10-24 21:51:16 -0300

    feat: adiciona script de implementação aleatória/automática da torre

---
+++ HelixJump Game/Assets/Scripts/HelixManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixManager : MonoBehaviour
+{
+    public GameObject[] helixRings;
+    private float ySpawn = 0; //coordenada para criar os aneis
+    private float ringsDistance = 5f; //distancia entre aneis
+    private int numberOfRings = 0; //quantidade de aneis da torre
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfRings = 12;
+        SpawnRing(0); //spawn do anel inicial
+        for (int i = 0; i < numberOfRings-1; i++)
+        {
+            SpawnRing(Random.Range(1, helixRings.Length-1));
+        }
+        SpawnRing(helixRings.Length - 1);
+        //Instantiate(helixRings[helixRings.Length - 1], transform.up * ySpawn, Quaternion.identity);
+    }
+
+    private void SpawnRing(int i)
+    {
+        GameObject go = Instantiate(helixRings[i], transform.up * ySpawn, Quaternion.identity);
+        go.transform.parent = transform; //anel pertence a torre
+        ySpawn -= ringsDistance; //atualiza a coordenada y para o proximo anel
+    }
+}
